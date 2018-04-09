6f9a30a495a40582e6d065e9ee03fbf81c47b813
Anna-Maria Hardi <gallenbe@hm.edu>
2018-04-09 20:12:02 +0200

    Added tests of class ByIdResultSelector.

--- src/test/java/io/jenkins/plugins/analysis/core/model/ByIdResultSelectorTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/model/ByIdResultSelectorTest.java
@@ -1,7 +1,6 @@
 package io.jenkins.plugins.analysis.core.model;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,30 +19,87 @@ import hudson.model.Run;
  */
 class ByIdResultSelectorTest {
 
-    String id = "1";
-
     @Test
-    void testNoActions() {
+    void testShouldEmptyIfActionsAreEmpty() {
+        ByIdResultSelector byIdResultSelector = new ByIdResultSelector("1");
+
         Run<?, ?> run = mock(Run.class);
-        when(run.getActions(ResultAction.class)).thenReturn(Collections.EMPTY_LIST);
-        ByIdResultSelector byIdResultSelector = new ByIdResultSelector(id);
+        when(run.getActions(ResultAction.class)).thenReturn(new ArrayList<>());
+
         Optional<ResultAction> actualResult = byIdResultSelector.get(run);
         assertThat(actualResult).isEmpty();
     }
 
-   /* @Test
-    void test1() {
+    @Test
+    void testOneShouldEmptyIfIdsAreDifferent() {
+        ByIdResultSelector byIdResultSelector = new ByIdResultSelector("1");
+
         ResultAction resultAction = mock(ResultAction.class);
+        when(resultAction.getId()).thenReturn("2");
+
         List<ResultAction> resAct = new ArrayList<>();
         resAct.add(resultAction);
 
         Run<?, ?> run = mock(Run.class);
         when(run.getActions(ResultAction.class)).thenReturn(resAct);
 
-        when(resultAction.getId()).thenReturn(id);
-        ByIdResultSelector byIdResultSelector = new ByIdResultSelector(id);
-        Optional<ResultAction> actualResult = byIdResultSelector.get(run);
-        //assertThat(actualResult).is(resultAction);
-    }*/
+        assertThat(byIdResultSelector.get(run)).isEmpty();
+    }
+
+    @Test
+    void testTwoShouldEmptyIfIdsAreDifferent() {
+        ByIdResultSelector byIdResultSelector = new ByIdResultSelector("2");
+
+        ResultAction resultAction = mock(ResultAction.class);
+        when(resultAction.getId()).thenReturn("3");
+
+        List<ResultAction> resAct = new ArrayList<>();
+        resAct.add(resultAction);
+
+        Run<?, ?> run = mock(Run.class);
+        when(run.getActions(ResultAction.class)).thenReturn(resAct);
+
+        assertThat(byIdResultSelector.get(run)).isEmpty();
+    }
+
+    @Test
+    void testThreeShouldEmptyIfIdsAreDifferent() {
+        ByIdResultSelector byIdResultSelector = new ByIdResultSelector("2");
+
+        ResultAction resultAction = mock(ResultAction.class);
+        when(resultAction.getId()).thenReturn("1");
+
+        List<ResultAction> resAct = new ArrayList<>();
+        resAct.add(resultAction);
+
+        Run<?, ?> run = mock(Run.class);
+        when(run.getActions(ResultAction.class)).thenReturn(resAct);
+
+        assertThat(byIdResultSelector.get(run)).isEmpty();
+    }
+
+
+    @Test
+    void testShouldFindSameIds() {
+        ByIdResultSelector byIdResultSelector = new ByIdResultSelector("1");
+
+        ResultAction resultAction = mock(ResultAction.class);
+        when(resultAction.getId()).thenReturn("1");
+
+        List<ResultAction> resAct = new ArrayList<>();
+        resAct.add(resultAction);
+
+        Run<?, ?> run = mock(Run.class);
+        when(run.getActions(ResultAction.class)).thenReturn(resAct);
+
+        assertThat(byIdResultSelector.get(run)).isPresent();
+    }
+
+    @Test
+    void testToString() {
+        ByIdResultSelector byIdResultSelector = new ByIdResultSelector("1");
+
+        assertThat(byIdResultSelector.toString()).isEqualTo("io.jenkins.plugins.analysis.core.views.ResultAction with ID 1");
+    }
 
 }
